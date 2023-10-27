fda0042424492847f092c7f15fe961ceb25516d8
Charles Humphrey <charles.humphrey@clockworkit.co.uk>
2023-10-27 09:50:39 +0100

    Minor change to the swagger sample.

--- Sample.MonoGame.Randomchaos.KestrelServer/Program.cs
+++ Sample.MonoGame.Randomchaos.KestrelServer/Program.cs
@@ -1,12 +1,11 @@
 ﻿
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Sample.MonoGame.Randomchaos.KestrelServer;
 using MonoGame.Randomchaos.KestrelServer.Models;
 
 using var game = new Sample.MonoGame.Randomchaos.KestrelServer.Game1();
 
-Host.CreateDefaultBuilder(args)
+_ = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<KestrelBackgroundService>();
